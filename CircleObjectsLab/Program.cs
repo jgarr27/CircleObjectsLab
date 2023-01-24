8929b87d4ba4f2b2b8ddd53c783c42d607016aac
jgarr27 <117048949+jgarr27@users.noreply.github.com>
2023-01-24 08:28:27 -0500

    first commit

---
+++ CircleObjectsLab/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using CircleObjectsLab;
+
+
+
+
+
+Console.WriteLine("Please enter a radius");
+int radius = int.Parse(Console.ReadLine());
+
+if (radius < 0)
+{
+    Console.WriteLine("Input is invalid");
+}
+else
+{
+    Console.WriteLine();
+}
+
+
+
---
+++ CircleObjectsLab/circle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleObjectsLab
+{
+    public class Circle
+    {
+        //Properties
+        public double Radius { get; set; }
+        public double Pi = Math.PI;
+        
+        
+
+        //Constructor
+        public Circle(double _radius, double _area, double Pi)
+        {
+            Radius = _radius;
+            
+        }
+        
+        
+            
+        
+        //methods
+        public double CalculateArea(decimal pi, double _radius)
+        {
+            return Math.PI * _radius * _radius;
+        }
+    
+        public double CalculateCircumference()
+        {
+            return 2 * Math.PI * Radius;
+        }
+        
+    }
+}
